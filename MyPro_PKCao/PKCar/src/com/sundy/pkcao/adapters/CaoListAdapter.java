99df54896fcba606a7d36457cc2e90f9f47529ab
Sundy_Xu <sundy_xu@mtelnet.com>
2015-04-16 16:27:35 +0800

    update share

--- MyPro_PKCao/PKCar/src/com/sundy/pkcao/adapters/CaoListAdapter.java
+++ MyPro_PKCao/PKCar/src/com/sundy/pkcao/adapters/CaoListAdapter.java
@@ -26,6 +26,7 @@ import com.sundy.pkcao.vo.Caodian_Img;
 import com.sundy.pkcao.vo.User;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -147,7 +148,12 @@ public class CaoListAdapter extends BaseAdapter {
                 holder.btn_share.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        showShare();
+                        HashMap<String, String> data = new HashMap<String, String>();
+                        data.put("title", "title");
+                        data.put("content", "content");
+                        data.put("img_url", "img_url");
+                        data.put("url", "url");
+                        CommonUtility.showShare(context, null);
                     }
                 });
             }
@@ -155,28 +161,6 @@ public class CaoListAdapter extends BaseAdapter {
         return view;
     }
 
-    private void showShare() {
-        ShareSDK.initSDK(context);
-        OnekeyShare oks = new OnekeyShare();
-        //关闭sso授权
-        oks.disableSSOWhenAuthorize();
-        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-        oks.setTitle(context.getString(R.string.share));
-        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-//        oks.setTitleUrl("http://sharesdk.cn");
-        // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
-        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
-        // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
-        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite(context.getString(R.string.app_name));
-        // 启动分享GUI
-        oks.show(context);
-    }
 
     public void likeCaodian(final AVObject caodian, View view) {
         final AQuery aQuery = new AQuery(view);
--- MyPro_PKCao/PKCar/src/com/sundy/pkcao/taker/CommonUtility.java
+++ MyPro_PKCao/PKCar/src/com/sundy/pkcao/taker/CommonUtility.java
@@ -22,6 +22,8 @@ import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.util.DisplayMetrics;
 import android.widget.Toast;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import com.sundy.pkcao.R;
 import com.sundy.pkcao.vo.User;
 
@@ -414,6 +416,28 @@ public class CommonUtility {
             e.printStackTrace();
         }
         return bitmap;
+    }
+
+    //分享
+    public static void showShare(Context context, HashMap<String, String> data) {
+        ShareSDK.initSDK(context);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(context.getString(R.string.share));
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText(data.get("title"));
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        oks.setImagePath(data.get("img_url"));//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl(data.get("url"));
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment(data.get("content"));
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(context.getString(R.string.app_name));
+        // 启动分享GUI
+        oks.show(context);
     }
 
 
